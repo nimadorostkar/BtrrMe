04ef04c4aad72b15e7e823d0e061e9c31898b5ee
Fan Wang <nimadorostkar97@gmail.com>
2024-09-14 17:52:49 +0330

    chat debug8

--- config/settings.py
+++ config/settings.py
@@ -123,8 +123,8 @@ ASGI_APPLICATION = "config.asgi.application"
 
 CHANNEL_LAYERS = {
     'default': {
-        #"BACKEND": "channels_redis.core.RedisChannelLayer",
-        "BACKEND": "channels_redis.pubsub.RedisPubSubChannelLayer",
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        #"BACKEND": "channels_redis.pubsub.RedisPubSubChannelLayer",
         "LOCATION": env("REDIS_URL"),
     },
 }
