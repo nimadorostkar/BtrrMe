c85bd021b2515e43572bdcb5bbcb0fe0fc0f8f11
Fan Wang <nimadorostkar97@gmail.com>
2024-09-14 16:50:32 +0330

    chat debug5

--- config/asgi.py
+++ config/asgi.py
@@ -5,16 +5,7 @@ from django.core.asgi import get_asgi_application
 from chat.routing import websocket_urlpatterns
 from chat.middleware import JWTAuthMiddleware
 
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
-'''
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
-})
-
-'''
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
--- config/settings.py
+++ config/settings.py
@@ -58,8 +58,6 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 
 ROOT_URLCONF = "config.urls"
-WSGI_APPLICATION = "config.wsgi.application"
-ASGI_APPLICATION = "config.asgi.application"
 AUTH_USER_MODEL = "accounts.User"
 AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend',]
 
@@ -130,7 +128,8 @@ CHANNEL_LAYERS = {
     },
 }
 
-
+#WSGI_APPLICATION = "config.wsgi.application"
+ASGI_APPLICATION = "config.asgi.application"
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
