883d8dfb4e6758773e34e42672eabd3f12eeb443
Fan Wang <nimadorostkar97@gmail.com>
2024-09-15 11:37:02 +0330

    new try...

--- chat/models.py
+++ chat/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 from accounts.models import User
 
-
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(max_length=255,null=True,blank=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
-    room_name = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True,null=True,blank=True)
+    room_name = models.CharField(max_length=255,null=True,blank=True)
 
     def __str__(self):
         return f"{self.user}: {self.content}"
