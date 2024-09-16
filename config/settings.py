9699d81c2ff731425678d58529d7a934ab1edfae
Fan Wang <nimadorostkar97@gmail.com>
2024-09-16 13:43:34 +0330

    push , asgi canceld

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ SITE_ID = 1
 
 # APP CONFIGURATION
 DJANGO_APPS = (
-    "daphne",
+    #"daphne",
     "chat",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -56,8 +56,8 @@ LOCAL_APPS = (
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 # END APP CONFIGURATION
 
-#WSGI_APPLICATION = "config.wsgi.application"
-ASGI_APPLICATION = "config.asgi.application"
+WSGI_APPLICATION = "config.wsgi.application"
+#ASGI_APPLICATION = "config.asgi.application"
 ROOT_URLCONF = "config.urls"
 AUTH_USER_MODEL = "accounts.User"
 AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend',]
@@ -122,8 +122,8 @@ CACHES = {
 
 CHANNEL_LAYERS = {
     'default': {
-        #"BACKEND": "channels_redis.core.RedisChannelLayer",
-        "BACKEND": "channels_redis.pubsub.RedisPubSubChannelLayer",
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        #"BACKEND": "channels_redis.pubsub.RedisPubSubChannelLayer",
         "LOCATION": env("REDIS_URL"),
     },
 }
