9bb369006595c8e895e6803404f58d09c5f46aac
Fan Wang <nimadorostkar97@gmail.com>
2024-10-04 17:00:21 +0330

    Update Workout

--- workout/models/workout.py
+++ workout/models/workout.py
@@ -11,7 +11,6 @@ class Equipment(models.Model):
         return str(self.name)
 
 
-
 class Workout(models.Model):
     place_choices = (("تمرین در منزل", "تمرین در منزل"), ("تمرین در باشگاه", "تمرین در باشگاه"))
     motion_status_choices = (("خوابیده", "خوابیده"), ("ایستاده", "ایستاده"), ("نشسته", "نشسته"), ("ترکیبی", "ترکیبی"))
