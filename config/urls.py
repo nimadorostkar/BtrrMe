fdcd77b0e48422542d06812c5e2a7581cdad88cc
Fan Wang <nimadorostkar97@gmail.com>
2024-11-01 12:55:50 +0330

    update swagger

--- config/urls.py
+++ config/urls.py
@@ -9,7 +9,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.conf import settings
 
-schema_view = get_schema_view(openapi.Info(title="Episyche Technologies",default_version='v1',),public=True,permission_classes=(permissions.AllowAny,),)
+schema_view = get_schema_view(openapi.Info(title="BtrrMe API documentation.",default_version='v1',),public=True,permission_classes=(permissions.AllowAny,),)
 
 
 urlpatterns = [
